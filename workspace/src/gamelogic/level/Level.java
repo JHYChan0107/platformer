13cdba70c8318941a8eee26cc2eb5867942529c8
JHYChan0107 <103075579+JHYChan0107@users.noreply.github.com>
2025-05-16 21:51:10 +0000

    "-m commit Submission: 5/16/25"

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -197,10 +197,100 @@ public class Level {
 	//Your code goes here! 
 	//Please make sure you read the rubric/directions carefully and implement the solution recursively!
 	private void water(int col, int row, Map map, int fullness) {
+		//make water (You’ll need modify this to make different kinds of water such as half water and quarter water)
+
+		if (col < 0 || col >= map.getWidth() || row < 0 || row >= map.getHeight()) return;
+
+		Tile current = map.getTiles()[col][row];
+
+		if (current.isSolid() || current instanceof Water) return;
+
+		String imageName;
+		if (fullness == 3) {
+			imageName = "Full_water";
+		} else if (fullness == 2) {
+			imageName = "Half_water";
+		} else if (fullness == 1) {
+			imageName = "Quarter_water";
+		} else {
+			imageName = "Falling_water";
+		}
+
+		Water w = new Water(col, row, tileSize, tileset.getImage(imageName), this, fullness);
+		map.addTile(col, row, w);
+
+
+        //check if we can go down
+		if (row + 1 < map.getTiles()[0].length) {
+			Tile below = map.getTiles()[col][row + 1];
+			if (!below.isSolid() && !(below instanceof Water)) {
+				water(col, row + 1, map, 0); // 0 = falling water
+				return; // Stop here; no side flow if falling
+			}
+		}
+
+        //if we can’t go down go left and right
+
+	if (row + 1 < map.getHeight()) {
+		Tile below = map.getTiles()[col][row + 1];
+		if (!below.isSolid() && !(below instanceof Water)) {
+			// We are over air; this should be falling water, don't spread sideways
+			map.addTile(col, row, new Water(col, row, tileSize, tileset.getImage("Falling_water"), this, 0));
+			water(col, row + 1, map, 0);
+			return;
+		}
+	}
+
+		// Flow right (if not falling)
+		if (col + 1 < map.getTiles().length) {
+			Tile right = map.getTiles()[col + 1][row];
+			if (!right.isSolid() && !(right instanceof Water)) {
+				if (row + 1 < map.getTiles()[0].length) {
+					Tile belowRight = map.getTiles()[col + 1][row + 1];
+					 if (fullness >= 1) {
+						water(col + 1, row, map, fullness - 1); // reduce fullness
+					} else {
+						water(col + 1, row, map, 1); // keep spreading quarter water
+					}
+				}
+			}
+		}
 		
+		// Flow left (if not falling)
+		
+		if (col - 1 >= 0) {
+			Tile left = map.getTiles()[col - 1][row];
+			if (!left.isSolid() && !(left instanceof Water)) {
+				if (row + 1 < map.getTiles()[0].length) {
+					Tile belowLeft = map.getTiles()[col - 1][row + 1];
+					if (fullness >= 1) {
+						water(col - 1, row, map, fullness - 1); // reduce fullness
+					} else {
+						water(col - 1, row, map, 1); // keep spreading quarter water
+					}
+				}
+			}
+		}
 	}
 
 
+
+		//right
+		/*
+		else {
+			if(col + 1 < map.getTiles().length && !(map.getTiles()[col + 1][row] instanceof Water) && !map.getTiles()[col + 1][row].isSolid()) {
+				water(col + 1, row, map, 3);
+			}
+			//left
+			if(col - 1 >= 0 && !(map.getTiles()[col - 1][row] instanceof Water) && !map.getTiles()[col + 1][row].isSolid()) {
+				water(col - 1, row, map, 3);
+			}
+		}
+		*/
+
+	
+
+	//#############################################################################################################
 
 	public void draw(Graphics g) {
 		g.translate((int) -camera.getX(), (int) -camera.getY());
